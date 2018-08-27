7f8b00f364f6c99b697e7278651cd12b08e1c79e
Robin Hill <rehill012@gmail.com>
2018-08-26 20:08:41 -0400

    Fix thumbnail dimensions

--- create_thumbnail.rb
+++ create_thumbnail.rb
@@ -4,7 +4,7 @@ def createThumbnail(path, image)
     image_path = path + "/" + image
     output_path = "output/" + path + "/thumbnails/" + image
     
-    %x( magick #{image_path} -resize '230' #{temp_path} )
+    %x( magick #{image_path} -resize '230x160^' #{temp_path} )
     %x( magick #{temp_path} -crop '230x160+0+0' -gravity center #{temp_path} )
     %x( mv #{temp_path} #{output_path} )
 end
